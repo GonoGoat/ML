9580695cbde693f7f3dc1ab5bf5eba9730fc5305
GonoGoat <gauth.vrsgn@outlook.be>
2023-12-05 12:50:39 +0100

    Examen PE

--- dissassemble.py
+++ dissassemble.py
@@ -1,68 +1,55 @@
 import pefile
+import peutils
+import re
 
-#the function takes two arguments, both are fetched from the exe file using
-#pefile. the first one is the list of all sections. The second one is the
-#address of the first instruction in the program
-def get_main_code_section(sections, base_of_code):
-    addresses = []
-    #get addresses of all sections
-    for section in sections:
-        print(section)
-        addresses.append(section.VirtualAddress)
+standardSections = [".text", ".bss", ".data",".rdata",".idata",".edata",".pdata",".rsrc",".reloc"]
+standardReg = "^"+ "$|^".join(standardSections) + "$"
+
+def getPeSectionsFeature(exe):
+    res = {"amountOfSections" : len(exe.sections)}
+    amountOfSuspiciousSections = 0
+    for section in exe.sections:
+        decodedName = section.Name.decode("ascii").rstrip('\x00')
+        if (re.search(standardReg,decodedName) == None):
+            amountOfSuspiciousSections += 1
+        #if any(se)
+    res['amountOfSuspiciousSections'] = amountOfSuspiciousSections
+    print(amountOfSuspiciousSections)
         
-    #if the address of section corresponds to the first instruction then
-    #this section should be the main code section
-    if base_of_code in addresses:    
-        return sections[addresses.index(base_of_code)]
-    #otherwise, sort addresses and look for the interval to which the base of code
-    #belongs
-    else:
-        addresses.append(base_of_code)
-        addresses.sort()
-        if addresses.index(base_of_code)!= 0:
-            return sections[addresses.index(base_of_code)-1]
-        else:
-            #this means we failed to locate it
-            return None
-      
-def fine_disassemble(exe):
-    #get main code section
-    main_code = get_main_code_section(exe.sections, exe.OPTIONAL_HEADER.BaseOfCode)
-    #define architecutre of the machine 
-    #md = Cs(CS_ARCH_X86, CS_MODE_32)
-    #md.detail = True
-    last_address = 0
-    last_size = 0
-    #Beginning of code section
-    begin = main_code.PointerToRawData
-    #the end of the first continuous bloc of code
-    end = begin+main_code.SizeOfRawData
-    while True:
-        #parse code section and disassemble it
-        data = exe.get_memory_mapped_image()[begin:end]
-        """for i in md.disasm(data, begin):
-            print(i)
-            last_address = int(i.address)
-            last_size = i.size"""
-        #sometimes you need to skip some bytes
-        begin = max(int(last_address),begin)+last_size+1
-        if begin >= end:
-            print("out")
-            break
-     
-exe_file_path = '0.exe'
+def getPeFeatures(exe):
+    res = {
+        "seemsPacked" : peutils.is_probably_packed(exe)
+    }
+    print(res)
 
-try:
+exe_file_path = '17.exe'
+exe = pefile.PE(exe_file_path)
+getPeSectionsFeature(exe)
+getPeFeatures(exe)
+
+# amountOfSections = getAmountOfSection(exe)
+
+'''try:
   #parse exe file
   exe = pefile.PE(exe_file_path)
-  #print(exe.sections)
+  sections = exe.parse_sections()
+  print(sections)
+  for section in sections.sections:
+      print(section)
   try:
     #call the function we created earlier
-    fine_disassemble(exe)
+    # fine_disassemble(exe)
     print("done")
   except:
     print('something is wrong with this exe file')
 except:
   print('pefile cannot parse this file')
-  
+  '''
 # objdump -d --no-addresses --no-show-raw-insn <file>
+
+# Analyse fichier PE
+# Extraction/Conversion de toutes les lignes de de caractères dans dissassembler
+# Liste des entrées registres modifiées après exécution
+# Analyse traffic réseau ?
+# Mesure entropie ?
+# Comaparaison signature
